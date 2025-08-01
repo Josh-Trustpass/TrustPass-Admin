5eda640e70ccceb69fe81cda4b646a59b32e3d3b
Josh-Trustpass <joshua@trustpass.uk>
2025-08-01 15:15:43 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { useAuth } from "@/hooks/useAuth";
 import NotFound from "@/pages/not-found";
 import EmployeeVerification from "@/pages/employee-verification";
@@ -59,9 +58,7 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
         <Router />
-      </TooltipProvider>
     </QueryClientProvider>
   );
 }
